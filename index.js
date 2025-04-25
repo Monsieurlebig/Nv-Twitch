4ad178f8b4ca2be5eecac6bb9ddbd842ca22c370
Monsieurlebig <monsieurlebigg@gmail.com>
2025-04-25 14:04:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const { Hono } = require('hono');
 const { serve } = require('@hono/node-server');
+const PCR = require("puppeteer-chromium-resolver");
 
 const app = new Hono();
 
 app.get('/', async (c) => {
   try {
-    const url = c.req.query('url') || 'https://apify.com';
+    const urlToPrint = c.req.query('url') || 'https://apify.com';
 
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+    const stats = await PCR();
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+      executablePath: stats.executablePath,
+      defaultViewport: { width: 1920, height: 945, deviceScaleFactor: 4 },
+    });
+
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // Timeout de 60 secondes
+    await page.setDefaultNavigationTimeout(60000); // Set timeout to 60 seconds
 
-    // Vérifier si le bouton "Commencer à regarder" est présent et cliquer dessus si nécessaire
+    await page.goto(urlToPrint, {
+      waitUntil: 'networkidle0',
+    });
+
+    // Wait for 5 seconds to ensure all content is loaded
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Clique sur le bouton si besoin
     const button = await page.$('[data-a-target="content-classification-gate-overlay-start-watching-button"]');
     if (button) {
       console.log("Bouton trouvé, on clique dessus...");
       await button.click();
-      await page.waitForTimeout(30000); // Petit délai pour que la vidéo charge
+      await page.waitForTimeout(1000); // Petit délai pour que la vidéo charge
     } else {
       console.log("Pas de bouton, on continue...");
     }
 
     // Attendre que la vidéo soit présente dans la page
-    await page.waitForFunction(() => document.querySelector('video')?.src, { timeout: 60000 });
+    await page.waitForFunction(() => document.querySelector('video')?.src, { timeout: 15000 });
 
     // Essayer d'extraire l'URL de la vidéo
     let videoUrl = await page.$eval('video', video => video.src).catch(() => null);
@@ -62,10 +77,10 @@ app.get('/', async (c) => {
     if (videoUrl) {
       console.log(`Clip URL: ${videoUrl}`);
       // Sauvegarde dans Apify Dataset
-      return c.json({ url: url, videoUrl: videoUrl });
+      return c.json({ url: urlToPrint, videoUrl: videoUrl });
     } else {
-      console.warn(`No video URL found for ${url}`);
-      return c.json({ url: url, videoUrl: null });
+      console.warn(`No video URL found for ${urlToPrint}`);
+      return c.json({ url: urlToPrint, videoUrl: null });
     }
 
     await browser.close();
